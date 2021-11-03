c83ef035a192598a55a974661e7e2ad0f855f6f8
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-11-03 20:04:30 +0530

    Create packinstall.py

---
+++ packinstall.py
@@ -0,0 +1,102 @@
+import sys
+import subprocess
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'pyautogui'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'pyjokes'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'tk'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'opencv-python'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'pillow'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'pyttsx3'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'datetime'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'SpeechRecognition'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'wikipedia'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+
+ 
+'''
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'os_sys'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+'''
