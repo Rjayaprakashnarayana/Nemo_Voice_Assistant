ececc09c06824da816c260ccb089773a700a484e
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2022-01-16 17:54:30 +0530

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:/Users/JAYA PRAKASH NARAYAN/AppData/Local/Programs/Python/Python37-32/python.exe" "C:\Users\JAYA PRAKASH NARAYAN\Github_repository#1\Nemo_Voice_Assistant\packinstall.py"
+"C:/Users/JAYA PRAKASH NARAYAN/AppData/Local/Programs/Python/Python37-32/python.exe" "C:\Users\JAYA PRAKASH NARAYAN\Github_repository#1\Nemo_Voice_Assistant\Nemo_Assistant.py"
+pause
